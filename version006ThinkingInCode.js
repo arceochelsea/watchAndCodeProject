7e06413ecb321da2d1597dbb949c8b98d43f219e
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-09 16:22:18 -0400

    Version 6 Thinking in Code

---
+++ version006ThinkingInCode.js
@@ -0,0 +1,68 @@
+//V6 REQUIREMENTS
+
+//.toggleAll: if everything is true, make everything false.
+// toggleAll: otherwise, make everything true.
+
+
+var todoList = { 
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+        console.log('your todo list is empty!');
+        } else {
+        console.log('My Todos:');
+        for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+        console.log('(x)', this.todos[i].todoText);
+        } else {
+        console.log('()', this.todos[i].todoText);
+        }
+      }
+    }
+  }, 
+    addTodo: function(todoText) {
+      this.todos.push( {
+        todoText: todoText,
+        completed: false 
+        } );
+      this.displayTodos();
+    }, 
+    changeTodo: function(position, todoText) {
+      this.todos[position].todoText = todoText; 
+      this.displayTodos();
+    }, 
+    deleteTodo: function(position) {
+      this.todos.splice(position, 1);
+      this.displayTodos();
+    },
+        toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }, 
+      toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+
+//get number of completed todos.
+        for (var i = 0; i < totalTodos; i++) {
+          if (this.todos[i].completed === true) {
+            completedTodos++;
+          }
+        }
+
+        //if everythings true make everything false.
+        if (completedTodos === totalTodos) {
+          for (var i = 0; i < totalTodos; i++) {
+            this.todos[i].completed = false;
+          }
+          //case 2// toggleAll: otherwise, make everything true.
+        } else {
+          for (var i = 0; i < totalTodos; i++) {
+            this.todos[i].completed = true;
+          }
+        }
+
+        this.displayTodos();
+      }
+    };
