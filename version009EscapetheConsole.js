b0ff86ff5201cee8289e83579106350d3a45a5c4
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-10 11:50:31 -0400

    Version 9 Escape the Console

---
+++ version009EscapetheConsole.js
@@ -0,0 +1,31 @@
+//v9 requirements
+
+//there should be an li element for ever todo 
+// each li element should contain .todoText
+// each li element should show completed
+
+
+<ul>
+</ul>
+
+
+var view = {
+    displayTodos: function() {
+        var todosUl = document.querySelector('ul');
+        todosUl.innerHTML = '';
+        for (var i = 0; i < todoList.todos.length; i++) {
+            var todoLi = document.createElement('li');
+            var todo = todoList.todos[i];
+            var todoTextWithCompletion = '';
+
+            if (todo.completed === true) {
+                todoTextWithCompletion = '(x)' + todo.todoText;
+            } else {
+                todoTextWithCompletion = '( )' + todo.todoText;
+            }
+
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+    }
+};
